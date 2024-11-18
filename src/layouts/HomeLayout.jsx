aafc97e2a8a1c2d2c7ecbbff5cb8c9f816b98171
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-18 08:52:45 +0600

    Add navbar.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto px-2 py-1">
-      <h1 className="text-3xl font-bold bg-red-400">Home Layout</h1>
-      <button className="btn btn-outline">Click</button>
+    <div className="max-w-screen-2xl mx-auto">
+      <Navbar />
     </div>
   );
 };
