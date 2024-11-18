a908862a78146705831477b6375822f8484b1ee3
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-18 08:35:32 +0600

    Add max width.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div className="max-w-screen-2xl mx-auto px-2 py-1">
       <h1 className="text-3xl font-bold bg-red-400">Home Layout</h1>
       <button className="btn btn-outline">Click</button>
     </div>
