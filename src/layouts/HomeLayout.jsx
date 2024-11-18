6258485a57f4dfcc05269a98bad77593853f8dc4
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-18 08:54:08 +0600

    Add outlet.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Navbar />
+      <Outlet />
     </div>
   );
 };
