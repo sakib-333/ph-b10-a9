fc810d1cb84e8b44bf013025080690c5319f66d8
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-18 09:52:42 +0600

    Add react icons.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,16 +7,23 @@ const Login = () => {
     <div className="px-2 max-w-screen-sm border mx-auto mt-4 p-4 rounded-xl bg-gray-200">
       <h1 className="font-bold text-center md:text-xl lg:text-2xl">Log in</h1>
       <form className="p-4 space-y-4">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="input input-bordered input-primary w-full"
-        />
-        <input
-          type={view ? "text" : "password"}
-          placeholder="Enter your password"
-          className="input input-bordered input-primary w-full"
-        />
+        <div>
+          <span className="label-text">Email</span>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="input input-bordered input-primary w-full"
+          />
+        </div>
+        <div>
+          <span className="label-text">Password</span>
+          <input
+            type={view ? "text" : "password"}
+            placeholder="Enter your password"
+            className="input input-bordered input-primary w-full"
+          />
+        </div>
+
         <button className="btn btn-success w-full text-white">Log in</button>
       </form>
       <div className="text-center">
