2b6957d79b37020cc39624a82dc6c48a8f8c6a7d
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-19 09:46:50 +0600

    Add campaign details form.

--- src/pages/DonationCampaignDetails.jsx
+++ src/pages/DonationCampaignDetails.jsx
@@ -7,11 +7,16 @@ const DonationCampaignDetails = () => {
 
   const { id } = useParams();
   useEffect(() => {
-    document.title = `Details ${id}`;
+    document.title = `Donation Details of ${id}`;
 
     const camp = campaigns.filter((item) => item.id.toString() === id);
     setCampaign(() => camp[0]);
   }, []);
+
+  const handleDonate = (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
 
   return (
     <div>
@@ -39,10 +44,6 @@ const DonationCampaignDetails = () => {
             </span>
           </div>
           <div className="mb-4">
-            <span className="font-semibold text-gray-800">Division:</span>{" "}
-            <span className="text-indigo-600">{campaign?.division}</span>
-          </div>
-          <div className="mb-4">
             <span className="font-semibold text-gray-800">Contact:</span>{" "}
             <a
               href={`mailto:${campaign?.contactInfo}`}
@@ -51,7 +52,75 @@ const DonationCampaignDetails = () => {
               {campaign?.contactInfo}
             </a>
           </div>
-          <button className="btn btn-primary w-full mt-4">Donate Now</button>
+          <div className="mb-4">
+            <span className="font-semibold text-gray-800">Division:</span>{" "}
+            <span className="text-indigo-600">{campaign?.division}</span>
+          </div>
+          <form
+            className="grid grid-cols-1 lg:grid-cols-2 gap-3 border rounded-xl p-4"
+            onSubmit={handleDonate}
+          >
+            <label className="form-control w-full lg:col-span-1">
+              <div className="label">
+                <span className="label-text font-bold">Quantity of items</span>
+              </div>
+              <input
+                type="text"
+                required
+                placeholder="e.g. 2 jackets, 3 blankets"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full lg:col-span-1">
+              <div className="label">
+                <span className="label-text font-bold">Pickup location</span>
+              </div>
+              <input
+                type="text"
+                required
+                placeholder="e.g. House 12, Road 5, Dhanmondi, Dhaka"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full lg:col-span-1">
+              <div className="label">
+                <span className="label-text font-bold">
+                  Item type &#40;e.g., blanket, jacket, sweater&#41;
+                </span>
+              </div>
+              <select required className="select select-bordered">
+                <option value="">Select any of them</option>
+                <option value="any">Any</option>
+                <option value="blanket">Blanket</option>
+                <option value="jacket">Jacket</option>
+                <option value="sweater">Sweater</option>
+              </select>
+            </label>
+            <label className="form-control lg:col-span-1">
+              <div className="label">
+                <span className="label-text font-bold">
+                  Additional notes &#40;optional&#41;
+                </span>
+              </div>
+              <textarea
+                className="textarea textarea-bordered"
+                rows={1}
+                placeholder="Write something about your donation &#40;optional&#41;"
+              ></textarea>
+            </label>
+            <div className="form-control">
+              <label className="label cursor-pointer justify-start gap-2">
+                <input type="checkbox" required className="checkbox" />
+                <span className="label-text">I agree</span>
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="w-full btn btn-success lg:col-span-2 text-white"
+            >
+              Donate
+            </button>
+          </form>
         </div>
       </div>
     </div>
